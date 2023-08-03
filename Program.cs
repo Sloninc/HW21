17320e3d535f67bedcd3ec3632c3097d9bf103cc
Andrei Ruk <sloninc@mail.ru>
2023-08-03 20:11:01 +0300

    Финальная версия

--- Program.cs
+++ Program.cs
@@ -5,14 +5,28 @@ namespace HW21
     {
         static void Main()
         {
-            var sw=Stopwatch.StartNew();
+            for (int i = 5; i <= 20; i *= 2)
+            {
+                FibonachiInvoke(i, FibonachiRecursion);
+                FibonachiInvoke(i, FibonachiСyclical);
+                Console.WriteLine(Environment.NewLine);
+            }
             Console.ReadLine();
         }
+        static void FibonachiInvoke(int n, Func<int, int> func)
+        {
+            var sw = Stopwatch.StartNew();
+            int result;
+            sw.Start();
+            result = func(n);
+            sw.Stop();
+            Console.WriteLine($"Для нахождения {n} члена последовательности Фибоначчи с помощью {func.Method.Name} " +
+                $"понадобилось {sw.ElapsedTicks} тактов.");
 
+        }
         static int FibonachiRecursion(int n)
         {
             if (n == 0 || n == 1) return n;
-
             return FibonachiRecursion(n - 1) + FibonachiRecursion(n - 2);
         }
         static int FibonachiСyclical(int n)
