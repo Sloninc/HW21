056b1e71a92ef10ec2acc355009e9dd7eb5487de
Andrei Ruk <sloninc@mail.ru>
2023-08-03 17:24:28 +0300

    Реализовано два метода числе фибоначчи.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,33 @@
-﻿namespace HW21
+﻿using System.Diagnostics;
+namespace HW21
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            var sw=Stopwatch.StartNew();
+            Console.ReadLine();
+        }
+
+        static int FibonachiRecursion(int n)
+        {
+            if (n == 0 || n == 1) return n;
+
+            return FibonachiRecursion(n - 1) + FibonachiRecursion(n - 2);
+        }
+        static int FibonachiСyclical(int n)
+        {
+            int result = 0;
+            int prev = 1;
+            int temp;
+
+            for (int i = 0; i < n; i++)
+            {
+                temp = result;
+                result = prev;
+                prev += temp;
+            }
+            return result;
         }
     }
 }
